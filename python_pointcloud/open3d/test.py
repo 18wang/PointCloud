658440671ee3d4d72fe2ed7ec83fd3456d305085
18wang <jswxingyu@outlook.com>
2022-01-14 18:07:33 +0800

    22.01.14.18

--- python_pointcloud/open3d/test.py
+++ python_pointcloud/open3d/test.py
@@ -1,13 +1,7 @@
-import numpy as np
-
-import trimesh
-
-# points = np.random.random((100, 3))
-
-# cloud = trimesh.PointCloud(points)
-cloudXYZ = np.loadtxt("../data/pz.txt")
-cloud = trimesh.PointCloud(cloudXYZ)
-
-# print(cloud.convex_hull)
-scene = trimesh.Scene([cloud.convex_hull])
-scene.show()
+while True:
+    try:
+        x = int(input("请输入一个数字: "))
+        break
+    except ValueError:
+        raise ValueError
+        print("您输入的不是数字，请再次尝试输入！")
