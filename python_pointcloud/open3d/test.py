99adc377197a0db2021f7eec7a532b239727f481
18wang <jswxingyu@outlook.com>
2022-01-18 21:50:37 +0800

    2022年1月18日

--- python_pointcloud/open3d/test.py
+++ python_pointcloud/open3d/test.py
@@ -1,7 +1,19 @@
-while True:
-    try:
-        x = int(input("请输入一个数字: "))
-        break
-    except ValueError:
-        raise ValueError
-        print("您输入的不是数字，请再次尝试输入！")
+def majorityElement(A, N):
+    # Your code here
+
+    dict_count = dict()
+
+    max = 1
+    result = A[1]
+    for i in A:
+        if dict_count.get(i):
+            dict_count[i] = dict_count[i] + 1
+            if dict_count[i] > max:
+                max = dict_count[i]
+                result = i
+        else:
+            dict_count[i] = 1
+
+    return result, max
+
+print(majorityElement([1, 2], 2))
